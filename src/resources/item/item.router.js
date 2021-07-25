1976abb593c6f5dadad34240c4b53854d2454f35
Christian Foster <fosterchris000@gmail.com>
2021-07-25 14:13:32 -0400

    start of lesson 3

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,16 +1,21 @@
 import { Router } from 'express'
 
+// could also put controller in the methods
+const controller = (req, res) => {
+  res.send({ message: 'Hello' })
+}
+
 const router = Router()
-router.route('/').get((req, res) => {
-  res.send('good')
-})
+router
+  .route('/')
+  .get(controller)
+  .post((req, res) => {
+    res.send(req.body)
+  })
 router
   .route('/:id')
   .get((req, res) => {
     res.send('from router ITem id')
-  })
-  .post((req, res) => {
-    res.send(req.body)
   })
   .put((req, res) => {
     res.send(req.body)
